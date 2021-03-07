18420f2a6b8d4752bdd0adf5a821576a6651e1f7
Tomoaki0726 <k.tomoaki.0726@gmail.com>
2021-03-07 17:55:33 +0900

    2021/3/7戻す

--- src/servlet/Login.java
+++ src/servlet/Login.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,7 +13,7 @@ import javax.servlet.http.HttpSession;
 import model.LoginLogic;
 import model.User;
 
-//@WebServlet("/Login")
+@WebServlet("/Login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
