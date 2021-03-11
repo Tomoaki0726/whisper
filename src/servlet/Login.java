83ee4c74fa48d105985ade485238914438391c94
Tomoaki0726 <k.tomoaki.0726@gmail.com>
2021-03-11 14:11:20 +0900

    3/11classesファイルの場所を変更

--- src/servlet/Login.java
+++ src/servlet/Login.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,7 +12,7 @@ import javax.servlet.http.HttpSession;
 import model.LoginLogic;
 import model.User;
 
-@WebServlet("/Login")
+//@WebServlet("/Login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
