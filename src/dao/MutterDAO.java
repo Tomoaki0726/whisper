dd1d6b78327527d39d2b35905583a513b9dba17c
Tomoaki0726 <k.tomoaki.0726@gmail.com>
2021-04-07 17:01:23 +0900

    2021/4/7 mysqlとHerokuの連携の為dao更新

--- src/dao/MutterDAO.java
+++ src/dao/MutterDAO.java
@@ -28,73 +28,92 @@ public class MutterDAO {
 
 
 
-//レコードを取得するメソッド
-public List<Mutter> findAll(){
+  //レコードを取得するメソッド
+    public List<Mutter> findAll(){
+    	Connection conn=null;
+    	List<Mutter> mutterList= new ArrayList<Mutter>();
+    	try{
+    		Class.forName("com.mysql.cj.jdbc.Driver");
+    		conn = getConnection();
+    	//	SELECT文の準備
+    		String sql =
+    		"SELECT ID,NAME,TEXT FROM MUTTER ORDER BY ID DESC";
+    		//SQL文をDBに届ける(SQLの送信)
+    		PreparedStatement pStmt = conn.prepareStatement(sql);
 
-	List<Mutter> mutterList= new ArrayList<Mutter>();
-	try{
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		try(Connection conn = getConnection()) {
+    		//SELECTを実行(結果の受け取り)
+    		ResultSet rs = pStmt.executeQuery();
 
-	//	SELECT文の準備
-		String sql =
-		"SELECT ID,NAME,TEXT FROM MUTTER ORDER BY ID DESC";
-		//SQL文をDBに届ける(SQLの送信)
-		PreparedStatement pStmt = conn.prepareStatement(sql);
+    		//SELECT文の結果をArrayListに格納
+    		while(rs.next()) {
+    			int id=rs.getInt("ID");
+    			String userName = rs.getString("NAME");
+    			String text = rs.getString("TEXT");
+    			Mutter mutter = new Mutter(id,userName,text);
+    			mutterList.add(mutter);
+    		}
 
-		//SELECTを実行(結果の受け取り)
-		ResultSet rs = pStmt.executeQuery();
+		 } catch (URISyntaxException e) {
+	         e.printStackTrace();
+	         return null;
+    	}catch (SQLException e) {
+    		e.printStackTrace();
+    		return null;
+    	}catch (ClassNotFoundException e) {
+    		e.printStackTrace();
+    		return null;
+    	}finally {
+    		//データベース切断
+    		if(conn != null) {
+    			try {
+    				conn.close();
+    			}catch (SQLException e) {
+    				e.printStackTrace();
+    				return null;
+    			}
+    		}
+    	}
+    	//はてな？↓どういう意味？mutterListを取得
+    	return mutterList;
+    }
 
-		//SELECT文の結果をArrayListに格納
-		while(rs.next()) {
-			int id=rs.getInt("ID");
-			String userName = rs.getString("NAME");
-			String text = rs.getString("TEXT");
-			Mutter mutter = new Mutter(id,userName,text);
-			mutterList.add(mutter);
-		}
-	  } catch (URISyntaxException e) {
-          e.printStackTrace();
-          return null;
-      } catch (SQLException e) {
-          e.printStackTrace();
-          return null;
-      }
-	}catch (ClassNotFoundException e) {
-		e.printStackTrace();
-		return null;
-	}
-	//はてな？↓どういう意味？mutterListを取得
-	return mutterList;
-}
+    //createメソッドでレコードを追加
+    public boolean create(Mutter mutter) {
+    	Connection conn =null;
+    	try {
+    		//データベースへ接続
+    		conn = getConnection();
 
-//createメソッドでレコードを追加
-public boolean create(Mutter mutter) {
-	try(Connection conn = getConnection()) {
+    		//INSERT文の準備(idは自動連番なので指定しなくてよい)
+    		String sql ="INSERT INTO MUTTER(NAME,TEXT) VALUES(?, ?)";
+    		PreparedStatement pStmt = conn.prepareStatement(sql);
+    		//INSERT文の「?」に使用する値を設定してSQLを完成
+    		pStmt.setString(1, mutter.getUserName());
+    		pStmt.setString(2, mutter.getText());
 
-		//INSERT文の準備(idは自動連番なので指定しなくてよい)
-		String sql ="INSERT INTO MUTTER(NAME,TEXT) VALUES(?, ?)";
-		PreparedStatement pStmt = conn.prepareStatement(sql);
-		//INSERT文の「?」に使用する値を設定してSQLを完成
-		pStmt.setString(1, mutter.getUserName());
-		pStmt.setString(2, mutter.getText());
+    		//INSERT文を実行(前処理済みのSQLを実行し，更新行数を返す)
+    		int result =pStmt.executeUpdate();
 
-		//INSERT文を実行(前処理済みのSQLを実行し，更新行数を返す)
-		int result =pStmt.executeUpdate();
-
-		if(result !=1) {
-			return false;
-		}
-	} catch (URISyntaxException e) {
-        e.printStackTrace();
-        return false;
-
-	}catch (SQLException e) {
-		e.printStackTrace();
-		return false;
-
-	}
-				return true;
-	}
-}
+    		if(result !=1) {
+    			return false;
+    		}
+    	} catch (URISyntaxException e) {
+            e.printStackTrace();
+            return false;
+    	}catch (SQLException e) {
+    		e.printStackTrace();
+    		return false;
+    	}finally {
+    		//データベース切断
+    		if(conn != null) {
+    			try {
+    				conn.close();
+    			}catch (SQLException e) {
+    				e.printStackTrace();
+    			}
+    		}
+    	}
+    				return true;
+    	}
+    }
 
